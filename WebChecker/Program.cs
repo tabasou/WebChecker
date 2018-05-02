55652228e72b0013cc249544cb9e39d80401a6d7
tabasou <tabasou@gmail.com>
2018-05-02 11:42:51 +0900

    呼ばないけど比較処理追加

--- WebChecker/Program.cs
+++ WebChecker/Program.cs
@@ -75,18 +75,17 @@ namespace WebChecker
             filePath = "Log/";
             filePath += midDomain();
 
-            /*  */
+            /* 出力ファイル名を作る */
             String fileName;
             fileName = createFileName();
 
-            /*  */
+            /* 出力Pathを完成させる */
             outputFilePath = System.IO.Path.Combine(filePath, fileName);
 
-            Console.WriteLine(outputFilePath);
+            //Console.WriteLine(outputFilePath);
 
-
+            /* 出力処理 */
             getWebPage(outputFilePath);
-
 
 
         }
@@ -158,6 +157,92 @@ namespace WebChecker
 
 
         #endregion
+
+    }
+
+    class DifferenceDitector
+    {
+        /* 指定されたPathのファイルを読み取って違いがあるか調べるクラス */
+        public String oldFilePath { get; set; }
+        public String newestFilePath { get; set; }
+
+        /* ファイルが存在するか確認する */
+        private bool CheckFileExist(){
+            bool bRtn = false;
+            if(System.IO.File.Exists(oldFilePath) && System.IO.File.Exists(newestFilePath)){
+                bRtn = true;
+            }else{
+                bRtn = false;
+                Console.WriteLine("File not found !!");
+            }
+            return bRtn;
+        }
+        /* テキスト内容の単純比較 */
+        public bool CompareRawText(){
+            bool bRtn = false;
+            /* ファイルの存在チェック */
+            if(bRtn = CheckFileExist()){
+                return bRtn;
+            }
+            /* ファイルの比較 */
+            bRtn = FileCompare(oldFilePath, newestFilePath);
+            return bRtn;
+        }
+
+        /* https://support.microsoft.com/ja-jp/help/320348/how-to-create-a-file-compare-function-in-visual-c */
+        // This method accepts two strings the represent two files to // compare. A return value of 0 indicates that the contents of the files
+        // are the same. A return value of any other value indicates that the 
+        // files are not the same.
+        private bool FileCompare(string file1, string file2)
+        {
+            int file1byte;
+            int file2byte;
+            FileStream fs1;
+            FileStream fs2;
+
+            // Determine if the same file was referenced two times.
+            if (file1 == file2)
+            {
+                // Return true to indicate that the files are the same.
+                return true;
+            }
+
+            // Open the two files.
+            fs1 = new FileStream(file1, FileMode.Open);
+            fs2 = new FileStream(file2, FileMode.Open);
+
+            // Check the file sizes. If they are not the same, the files 
+            // are not the same.
+            if (fs1.Length != fs2.Length)
+            {
+                // Close the file
+                fs1.Close();
+                fs2.Close();
+
+                // Return false to indicate files are different
+                return false;
+            }
+
+            // Read and compare a byte from each file until either a
+            // non-matching set of bytes is found or until the end of
+            // file1 is reached.
+            do
+            {
+                // Read one byte from each file.
+                file1byte = fs1.ReadByte();
+                file2byte = fs2.ReadByte();
+            }
+            while ((file1byte == file2byte) && (file1byte != -1));
+
+            // Close the files.
+            fs1.Close();
+            fs2.Close();
+
+            // Return the success of the comparison. "file1byte" is 
+            // equal to "file2byte" at this point only if the files are 
+            // the same.
+            return ((file1byte - file2byte) == 0);
+        }
 
     }
     #endregion
